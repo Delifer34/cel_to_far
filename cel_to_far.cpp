f8d5bc7adfdf45b75e6b50e443fc72fb3f3ce244
Delifer <96007289+Delifer34@users.noreply.github.com>
2022-01-13 16:20:41 +0700

    Add files via upload

---
+++ cel_to_far.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    float Far, Cel;
+    cout<<"Far = "; cin>>Far;
+    Cel = (Far - 32)5/9;
+    cout<<"Cel = "<<Cel<<endl;
+    cout<<"Cel = "; cin>>Cel;
+    Far = Cel9/5 + 32;
+    cout<<"Far = "<<Far<<endl;
+    return 0;
+}
